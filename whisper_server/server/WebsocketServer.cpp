0f54e45e662ea56b57f0caccdd662c65c0f3662e
YunboChow <96528637+YunboChow@users.noreply.github.com>
2023-11-06 19:57:15 +0900

    Update WebsocketServer.cpp

--- whisper_server/server/WebsocketServer.cpp
+++ whisper_server/server/WebsocketServer.cpp
@@ -24,3 +24,14 @@ string WebsocketServer::stringifyJson(const Json::Value& val)
 
     return Json::writeString(wbuilder, val);
 }
+
+WebsocketServer::WebsocketServer()
+{
+    //Wire up our event handlers
+    this->endpoint.set_open_handler(std::bind(&WebsocketServer::onOpen, this, std::placeholders::_1));
+    this->endpoint.set_close_handler(std::bind(&WebsocketServer::onClose, this, std::placeholders::_1));
+    this->endpoint.set_message_handler(std::bind(&WebsocketServer::onMessage, this, std::placeholders::_1, std::placeholders::_2));
+
+    //Initialise the Asio library, using our own event loop object
+    this->endpoint.init_asio(&(this->eventLoop));
+}
