d55936b9ceeb74004ee65c672fe6ee964c7ace32
YunboChow <96528637+YunboChow@users.noreply.github.com>
2023-11-06 19:57:01 +0900

    Update WebsocketServer.cpp

--- whisper_server/server/WebsocketServer.cpp
+++ whisper_server/server/WebsocketServer.cpp
@@ -6,3 +6,21 @@
 
 //The name of the special JSON field that holds the message type for messages
 #define MESSAGE_FIELD "MessageType"
+
+Json::Value WebsocketServer::parseJson(const string& json)
+{
+    Json::Value root;
+    Json::Reader reader;
+    reader.parse(json, root);
+    return root;
+}
+
+string WebsocketServer::stringifyJson(const Json::Value& val)
+{
+    //When we transmit JSON data, we omit all whitespace
+    Json::StreamWriterBuilder wbuilder;
+    wbuilder["commentStyle"] = "None";
+    wbuilder["indentation"] = "";
+
+    return Json::writeString(wbuilder, val);
+}
