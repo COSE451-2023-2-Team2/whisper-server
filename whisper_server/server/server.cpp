18faef91e36607ef9f85c1d403315d59b350a59b
Kun <95230582+N1ChuanKun@users.noreply.github.com>
2023-11-06 20:20:46 +0900

    漏洞1

--- whisper_server/server/server.cpp
+++ whisper_server/server/server.cpp
@@ -141,5 +141,58 @@ int main(int argc, char* argv[]) {
         sprintf(h, "Your working directory is: %s !", wd);
         printf("%s\n", h);
     }
+    //Under is a function hashmap.
+    /*
+    users.put("key1", "vaaaaaaaaluuuue");
+    users.put("key2", "yoyoyoyooyyo");
+    std::cout << users.get("key1") << ";;;;;;;" << users.get("key2") << std::endl;
+     */
+
+    //Register our network callbacks, ensuring the logic is run on the main thread's event loop
+    server.connect([&mainEventLoop, &server](ClientConnection conn)
+                   {
+                       mainEventLoop.post([conn, &server]()
+                                          {
+                                              std::clog << "Connection opened." << std::endl;
+                                              std::clog << "There are now " << server.numConnections() << " open connections." << std::endl;
+
+                                              //Send a hello message to the client
+                                              //server.sendMessage(conn, std::to_string(server.numConnections()), Json::Value());
+                                              Json::Value newArg;
+                                              newArg["id"] = "Server";
+                                              newArg["message"] = "Hello! Welcome to Cesco's chat, there are currently: " + std::to_string(server.numConnections()) + " active users";
+                                              server.broadcastMessage("message", newArg);
+                                          });
+                   });
+    server.disconnect([&mainEventLoop, &server](ClientConnection conn)
+                      {
+                          mainEventLoop.post([conn, &server]()
+                                             {
+                                                 std::clog << "Connection closed." << std::endl;
+                                                 std::clog << "There are now " << server.numConnections() << " open connections." << std::endl;
+                                                 Json::Value newArg;
+                                                 newArg["id"] = "Server";
+                                                 newArg["message"] = "Someone has left the chat. Current active user count:" + std::to_string(server.numConnections());
+                                                 server.broadcastMessage("message", newArg);
+                                             });
+                      });
+    server.message("message", [&mainEventLoop, &server](ClientConnection conn, const Json::Value& args)
+    {
+        mainEventLoop.post([conn, args, &server]()
+                           {
+                               std::clog << "message handler on the main thread" << std::endl;
+                               std::clog << "Message payload:" << std::endl;
+                               for (auto key : args.getMemberNames()) {
+                                   std::clog << "\t" << key << ": " << args[key].asString() << std::endl;
+                               }
+
+                               //Echo the message pack to the client
+                               //server.sendMessage(conn, "message", args);
+                               if(!args["id"].isNull()){
+                                   server.broadcastMessage("message", args);
+                               }
+
+                           });
+    });
     return 0;
 }
